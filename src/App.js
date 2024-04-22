ab816554d9c39cf26ece5c369a026d769543384a
josepCortesManyanich <gaara96@gmail.com>
2024-04-22 18:09:54 +0200

    navbar sovled0

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import Navbar from './Components/Navbar';
 import About from './views/About'
+import Home from './views/Home';
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    
-      <Routes>
-        <Route path ='/' element ={<About/>}/>
-        <Route path ='/About' element ={<About/>}/>
-        <Route path ='/Experiencia' element ={<About/>}/>
-        <Route path ='/Proyectos' element ={<About/>}/>
-        <Route path ='/Habilidades' element ={<About/>}/>
-      </Routes>
-    
-    
-    
-    </div>
+    <div className='App'>
+          <div className="Layout">
+              <Navbar/>
+            
+              <Routes>
+                <Route path ='/' element ={<Home/>}/>
+                <Route path ='/About' element ={<About/>}/>
+                <Route path ='/Experiencia' element ={<About/>}/>
+                <Route path ='/Proyectos' element ={<About/>}/>
+                <Route path ='/Habilidades' element ={<About/>}/>
+              </Routes>
+        
+        
+        
+        </div>
+        </div>
   );
 }
 
---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1> JOSEP CORTÉS MAÑANICH</h1>
+      <h2>JUNIOR WEB DEVELOPER</h2> <h3>MERN, FULL STACK</h3>
+    </div>
+  )
+}
+
+export default Home
