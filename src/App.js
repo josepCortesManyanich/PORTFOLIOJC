1b2c69fae249ddb4679281656622d7939f561d06
josepCortesManyanich <gaara96@gmail.com>
2024-04-23 20:44:30 +0200

    doing experience

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './views/About'
 import Home from './views/Home';
 import Skills from './views/Skills';
 import Footer from './Components/Footer';
+import Experience from './views/Experience';
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
               <Routes>
                 <Route path ='/' element ={<Home/>}/>
                 <Route path ='/About' element ={<About/>}/>
-                <Route path ='/Experiencie' element ={<About/>}/>
+                <Route path ='/Experiencie' element ={<Experience/>}/>
                 <Route path ='/Proyects' element ={<About/>}/>
                 <Route path ='/Skills' element ={<Skills/>}/>
               </Routes>
--- src/views/Experience.jsx
+++ src/views/Experience.jsx
@@ -1,9 +1,89 @@
 import React from 'react'
 
 const Experience = () => {
-  return (
+  
+  
+  
+  
+return (
     <div>
-      
+        <div className='projects'>
+            <ul className='projects-list'>
+                <li className='projects-item'>
+                    <div className='item'> 
+                        <img src="https://res.cloudinary.com/josepcortes/image/upload/v1713895785/Captura_desde_2024-04-23_20-09-09_wwttu9.png" alt="" /> 
+                            <ul>
+                                
+                                <li><p>-My first project, Mario Game, </p></li>
+                                <li><p>-Game where Mario will try to survive, and catch all the posible "languajes"</p></li>
+                                
+                            </ul>
+  
+                            <button> <a href="https://github.com/josepCortesManyanich/Project-1"><img src="https://res.cloudinary.com/josepcortes/image/upload/v1713896520/64e79ca5aff2fb7295bfddf9_github-que-es_vwvfsc.jpg" alt="" /></a></button>
+                            <div className='lenguajes'> <p>HTML5</p> <p>CSS3</p> <p>JS</p></div>
+                    </div>
+
+                   
+                </li>
+
+                <li className='projects-item'>
+                    <div className='item'> 
+                        <img src="https://res.cloudinary.com/josepcortes/image/upload/v1713897737/Captura_desde_2024-04-23_20-39-34_nycop4.png" alt="" /> 
+                            <ul>
+                                
+                                <li><p>-This is a project developed with my friend Ali as the project for the second module at Ironhack </p></li>
+                                <li><p>-Online store of CBD, which stands for cannabidiol, where user will able to buy our products from the store, add products to cart, delete products from the cart and pay at the checkout page with a credit card.</p></li>
+                                
+                            </ul>
+  
+                            <button> <a href="https://github.com/josepCortesManyanich/proyecto2"><img src="https://res.cloudinary.com/josepcortes/image/upload/v1713896520/64e79ca5aff2fb7295bfddf9_github-que-es_vwvfsc.jpg" alt="" /></a></button>
+                            <div className='lenguajes'> <p>HTML5</p> <p>CSS3</p> <p>JS</p> <p>MONGO</p> <p>NODE</p></div>
+                    </div>
+
+                   
+                </li>
+
+                <li className='projects-item'>
+                    <div className='item'> 
+                        <img src="https://res.cloudinary.com/josepcortes/image/upload/v1713895785/Captura_desde_2024-04-23_20-09-09_wwttu9.png" alt="" /> 
+                            <ul>
+                                
+                                <li><p>-My first project, Mario Game, </p></li>
+                                <li><p>-Game where Mario will try to survive, and catch all the posible "languajes"</p></li>
+                                
+                            </ul>
+  
+                            <button> <a href="https://github.com/josepCortesManyanich/Project-1"><img src="https://res.cloudinary.com/josepcortes/image/upload/v1713896520/64e79ca5aff2fb7295bfddf9_github-que-es_vwvfsc.jpg" alt="" /></a></button>
+                            <div className='lenguajes'> <p>HTML5</p> <p>CSS3</p> <p>JS</p></div>
+                    </div>
+
+                   
+                </li>
+
+
+                <li className='projects-item'>
+                    <div className='item'> 
+                        <img src="https://res.cloudinary.com/josepcortes/image/upload/v1713895785/Captura_desde_2024-04-23_20-09-09_wwttu9.png" alt="" /> 
+                            <ul>
+                                
+                                <li><p>-My first project, Mario Game, </p></li>
+                                <li><p>-Game where Mario will try to survive, and catch all the posible "languajes"</p></li>
+                                
+                            </ul>
+  
+                            <button> <a href="https://github.com/josepCortesManyanich/Project-1"><img src="https://res.cloudinary.com/josepcortes/image/upload/v1713896520/64e79ca5aff2fb7295bfddf9_github-que-es_vwvfsc.jpg" alt="" /></a></button>
+                            <div className='lenguajes'> <p>HTML5</p> <p>CSS3</p> <p>JS</p></div>
+                    </div>
+
+                   
+                </li>
+
+
+
+                
+            </ul>
+
+        </div>
     </div>
   )
 }
