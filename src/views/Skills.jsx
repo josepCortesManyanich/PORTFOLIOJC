cb60aac637a522c0adcf2e98ab007b727fb80ad4
josepCortesManyanich <gaara96@gmail.com>
2024-04-23 16:18:59 +0200

    more or less skills solved

--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -5,26 +5,34 @@ const Skills = () => {
 
     <div className='skills-container'>
         <div className='skills'>
-        <h3 className='title1'>I love to learn new things and experiment with new technologies.</h3>
+                <h1>MY SKILLS</h1>
+                <h3 className='title11'>I love to learn new things and experiment with new technologies.</h3>
                 <h4 className='title2'>These are some of the major languages, technologies, tools and platforms I have worked with:</h4>
             <div className='skills-item'>
-                
+                <h3 className='title1'>Languages & Databases</h3>
                 <ul>
-                   <li><img alt='HTML5' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824451/html_hjfzyu.png' /></li> 
-                    <li><img alt='CSS3' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824479/descarga_1_mwrnmv.png' /></li>
-                    <li><img alt='JAVASCRIPT' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824459/descarga_2_vubqwe.png' /></li>
-                    <li><img alt='MONGO DB' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824490/descarga_4_gxrpob.png' /></li>
-                    <li><img alt='MY SQL' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824470/descarga_3_b9wwrf.jpg' /></li>
+                   <li><img className='html-image' alt='HTML5' src='https://res.cloudinary.com/josepcortes/image/upload/v1713881218/Captura_desde_2024-04-23_16-05-35-removebg-preview_hex0cj.png' /></li> 
+                    <li><img className='css-image'alt='CSS3' src='https://res.cloudinary.com/josepcortes/image/upload/v1713881248/Captura_desde_2024-04-23_16-04-31-removebg-preview_m02tab.png' /></li>
+                    <li><img className='javascript-image' alt='JAVASCRIPT' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824459/descarga_2_vubqwe.png' /></li>
+                    <li><img className='mongo-image' alt='MONGO DB' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824490/descarga_4_gxrpob.png' /></li>
+                    <li><img className='mysql-image' alt='MY SQL' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824470/descarga_3_b9wwrf.jpg' /></li>
                 </ul>
             </div>
         
             
             <div className='skills-item'>
-                <h3 className='title1'>Languages & Databases</h3>
+                <h3 className='title1'>Frameworks & Technologies</h3>
+                <ul>
+                        <li><img className='html-image' alt='HTML5' src='https://res.cloudinary.com/josepcortes/image/upload/v1713881218/Captura_desde_2024-04-23_16-05-35-removebg-preview_hex0cj.png' /></li> 
+                        <li><img className='css-image'alt='CSS3' src='https://res.cloudinary.com/josepcortes/image/upload/v1713881248/Captura_desde_2024-04-23_16-04-31-removebg-preview_m02tab.png' /></li>
+                        <li><img className='javascript-image' alt='JAVASCRIPT' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824459/descarga_2_vubqwe.png' /></li>
+                        <li><img className='mongo-image' alt='MONGO DB' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824490/descarga_4_gxrpob.png' /></li>
+                        <li><img className='mysql-image' alt='MY SQL' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824470/descarga_3_b9wwrf.jpg' /></li>
+                </ul>
             </div>
 
             <div className='skills-item'>
-                <h3 className='title1'>Frameworks & Technologies</h3>
+                
 
 
             </div>
@@ -32,7 +40,13 @@ const Skills = () => {
 
             <div className='skills-item'>
                 <h3 className='title1'>Tools & Platforms</h3>
-
+                <ul>
+                   <li><img className='html-image' alt='HTML5' src='https://res.cloudinary.com/josepcortes/image/upload/v1713881218/Captura_desde_2024-04-23_16-05-35-removebg-preview_hex0cj.png' /></li> 
+                    <li><img className='css-image'alt='CSS3' src='https://res.cloudinary.com/josepcortes/image/upload/v1713881248/Captura_desde_2024-04-23_16-04-31-removebg-preview_m02tab.png' /></li>
+                    <li><img className='javascript-image' alt='JAVASCRIPT' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824459/descarga_2_vubqwe.png' /></li>
+                    <li><img className='mongo-image' alt='MONGO DB' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824490/descarga_4_gxrpob.png' /></li>
+                    <li><img className='mysql-image' alt='MY SQL' src='https://res.cloudinary.com/josepcortes/image/upload/v1713824470/descarga_3_b9wwrf.jpg' /></li>
+                </ul>
             </div>
 
 
